477a33c1155c8e45c52688f5cef17d624039e6d8
MrVicho13 <vicho0202@gmail.com>
2025-03-23 22:49:57 -0300

    add: custom icons system

--- __init__.py
+++ __init__.py
@@ -14,12 +14,16 @@ def reload_inzoider():
 if "auto_load" in locals():
     reload_inzoider()
 
+from . import icons_load
 from . import auto_load
 
 auto_load.init()
 
 def register():
+    icons_load.init_icons()
+    icons_load.load_icons()
     auto_load.register()
 
 def unregister():
     auto_load.unregister()
+    icons_load.unregister_icons()
